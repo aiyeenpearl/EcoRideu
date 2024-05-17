bf3e4993c6737e7c56e155286633830db6d5e216
Aiyeen Pearl J. Cuervo <128879913+aiyeenpearl@users.noreply.github.com>
2024-05-17 16:52:23 +0800

    Create orders.js

---
+++ Dashboard/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: '0001',
+        productNumber: '200W',
+        paymentStatus: '05/13/2024',
+        status: '06:49 PM'
+    },
+    {
+        productName: '0013',
+        productNumber: '250W',
+        paymentStatus: '05/01/2024',
+        status: '07:08 AM'
+    },
+    {
+        productName: '0010',
+        productNumber: '200W',
+        paymentStatus: '04/28/2024',
+        status: '12:28 PM'
+    },
+]
